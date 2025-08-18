7ad586581d2eebc7de3aef32f7bdbd8f706dd095
ManjuN <raidersearch3@gmail.com>
2025-08-18 12:15:34 +0530

    reverting the Dashboard.js

--- Frontend/employee-management-frontend/src/pages/Dashboard.js
+++ Frontend/employee-management-frontend/src/pages/Dashboard.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Dashboard = () => {
-  return <div>Welcome to the Employee Management system!</div>;
+  return <div>Welcome to the Dashboard!</div>;
 };
 
 export default Dashboard;
